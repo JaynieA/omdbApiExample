1158125beecc25e0b55aa45b884c5a8880a01b23
Jaynie Anderson <mrs.jaynie.anderson@gmail.com>
2016-12-01 07:40:26 -0600

    refactored displaySearchResults to allow movies to update on dom after delete, removing whitespace. Refactored fadeout to also remove the element

--- scripts/script.js
+++ scripts/script.js
@@ -1,12 +1,11 @@
 var movies = [];
 $(document).ready(function() {
-  //give focus to input on load
+  //give focus to search input on load
   var searchInput = $('#searchIn');
   searchInput.focus();
-
+  // form submit event
   $( '#searchForm' ).on('submit', function(e) {
     e.preventDefault();
-    console.log( 'on click button' );
     //get user input for the search
     var movieTitle = $( '#searchIn' ).val();
     // alert the user if they left the input empty when trying to search
@@ -26,43 +25,49 @@ $(document).ready(function() {
         displaySearchResults(data);
       }
     }); // end ajax
-    //clear search input value
+    //clear search input value and remove focus from input
     searchInput.val('');
-    //remove focus from search input after search
     searchInput.blur();
   }); // end #searchForm onsubmit
 
   var displaySearchResults = function(data) {
     console.log('in displaySearchResults');
     for (var i = 0; i < data.Search.length; i++) {
-      //push (unshift) movies into front of movies array
+      //push (unshift) movies into FRONT of movies array
       movies.unshift(data.Search[i]);
-    }
-    var movieText = '';
-    // iterate through movies array, parse data and display on the DOM
-    for (var y = 0; y < movies.length; y++) {
-      movieText += '<div class="col-sm-3"><div class="movie text-center" data-id="'+ movies[y].imdbID + '"><p><strong> ' + movies[y].Title + ', ' + movies[y].Year + '</strong></p>';
-      movieText += '<img src="' + movies[y].Poster + '" class="thumbnail img-responsive posterImg"/>';
-      movieText += '<div class="movie-footer"><button class="btn btn-default btn-sm btn_delete"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button></div></div></div>';
-    }
-    console.log(movies);
-    // display the data on the DOM
-    $('#outputDiv').html(movieText);
+    } // end for
+    updateMoviesOnDOM();
   }; // end displaySearchResults
 }); // end doc ready
 
+var updateMoviesOnDOM = function() {
+  var movieText = '';
+  // iterate through movies array, parse data and display on the DOM
+  for (var y = 0; y < movies.length; y++) {
+    movieText += '<div class="col-sm-3"><div class="movie text-center" data-id="'+ movies[y].imdbID + '"><p><strong> ' + movies[y].Title + ', ' + movies[y].Year + '</strong></p>';
+    movieText += '<img src="' + movies[y].Poster + '" class="thumbnail img-responsive posterImg"/>';
+    movieText += '<div class="movie-footer"><button class="btn btn-default btn-sm btn_delete"><span class="glyphicon glyphicon-trash" aria-hidden="true"></span></button></div></div></div>';
+  } // end for
+  console.log(movies);
+  // display the data on the DOM
+  $('#outputDiv').html(movieText);
+}; // end updateMoviesOnDOM
+
 //button click event for delete button
 $( document ).on( 'click', '.btn_delete', function(event) {
-  // fadeOut the removed movie from the DOM
-  $(this).closest('.movie').fadeOut();
-  // iterate through movies array to match omdbID with data attribute of movie to delete
+  // fadeOut and remove movie from the DOM
+  $(this).closest('.movie').fadeOut(500, function() {
+    $(this).remove();
+    updateMoviesOnDOM();
+  }); // end fadeOut
+  // iterate through movies array to find the movie with the same imdbID as the deleted movie's data attribute
   for (var i = 0; i < movies.length; i++) {
     if (movies[i].imdbID === $(this).closest('.movie').data().id) {
       var index = movies.indexOf(movies[i]);
-      //remove the movie from movies array
+      //remove the matching movie from movies array
       if (index > -1) {
           movies.splice(index, 1);
       }
     }
-  }
+  } // end for
 }); // end on click for .btn_delete
